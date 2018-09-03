23fe209d48da54c7036d48ef9464635961359e3a
xjboss <i@xjboss.net>
2018-09-03 20:35:32 +0800

    Fix callbacks

--- src/main/java/net/xjboss/jminiblink/natives/callbacks/wkeNavigationCallback.java
+++ src/main/java/net/xjboss/jminiblink/natives/callbacks/wkeNavigationCallback.java
@@ -4,5 +4,5 @@ import com.sun.jna.Callback;
 import com.sun.jna.Pointer;
 
 public interface wkeNavigationCallback extends Callback {
-    void invoke(Pointer webView, Pointer param, int navigationType, Pointer url);
+    boolean invoke(Pointer webView, Pointer param, int navigationType, Pointer url);
 }
